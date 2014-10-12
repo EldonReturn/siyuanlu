9f903d25be8b0f5e08c36205b1a581d502451d3a
He Gu Yi <ezcommerce@outlook.com>
2014-10-12 21:10:04 +0800

    Updated

--- index.php
+++ index.php
@@ -1,13 +1,61 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 <body>
 
-<ul>
-	<li>
-	</li>
-</ul>
+<div class="news_area">
+	<ul class="news_block">
+		<li class="news_item news_item_0">
+			<div class="news_item_container">
+				<a href="#">
+					<img src="images/sample.jpg" class="news_item_image">
+					<h3 class="news_item_title">2015 春季时装秀掠影</h3>
+				</a>
+				<p class="news_item_text">备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。</p>
+				<div class="news_item_stat">10月18日 | 时尚</div>
+			</div>
+		</li>
+		<li class="news_item news_item_1">
+			<div class="news_item_container">
+				<a href="#">
+					<img src="images/sample.jpg" class="news_item_image">
+					<h3 class="news_item_title">2015 春季时装秀掠影</h3>
+				</a>
+				<div class="news_item_stat">10月18日 | 时尚</div>
+			</div>
+		</li>
+		<li class="news_item news_item_2">
+			<div class="news_item_container">
+				<a href="#">
+					<img src="images/sample.jpg" class="news_item_image">
+					<h3 class="news_item_title">2015 春季时装秀掠影</h3>
+				</a>
+				<div class="news_item_stat">10月18日 | 时尚</div>
+			</div>
+		</li>
+		<li class="news_item news_item_3">
+			<div class="news_item_container">
+				<a href="#">
+					<img src="images/sample.jpg" class="news_item_image">
+					<h3 class="news_item_title">2015 春季时装秀掠影</h3>
+				</a>
+				<div class="news_item_stat">10月18日 | 时尚</div>
+			</div>
+		</li>
+		<li class="news_item news_item_4">
+			<div class="news_item_container">
+				<a href="#">
+					<img src="images/sample.jpg" class="news_item_image">
+					<h3 class="news_item_title">2015 春季时装秀掠影</h3>
+				</a>
+				<div class="news_item_stat">10月18日 | 时尚</div>
+			</div>
+		</li>
+	</ul>
+</div>
 
 </body>
 </html>
