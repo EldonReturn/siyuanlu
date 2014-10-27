94c2d1e9f5e36e579dde34c5a39a75b828185d39
He Gu Yi <eldonreturn@gmail.com>
2014-10-27 18:02:19 +0800

    Dojo Init

--- resources/home/js/marquee.js
+++ resources/home/js/marquee.js
@@ -1,15 +1,19 @@
 (function($){
 	$.Marquee = function(){
-		var root = $('<ul class="marquee_box"></ul>'),
+		var root = $('<div class="marquee"></div>'),
+			marqueeBox = $('<ul class="marquee_box"></ul>'),
+			masker = $('<div class="mask"><span></span><span></span></div>'),
 			data = [{img:'resources/home/images/sample_large.jpg'}, {img:'resources/home/images/sample_large.jpg'}],
 			node, image, len = data.length,
 			scrWidth = $(document).width();
 
+		root.append(masker);
+		root.append(marqueeBox);
 		for(var i = 0; i < len; i++){
 			node = $('<li class = "slide"></li>');
 			image = $('<img src="' + data[i].img + '" />');
 			node.append(image);
-			root.append(node);
+			marqueeBox.append(node);
 			image.load(function(){
 				node.css('left', (scrWidth - node.width()) / 2);
 			});
