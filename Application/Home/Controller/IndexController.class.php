3e9c4581df47647e1fd181d37cf72111b065a01d
He Gu Yi <eldonreturn@gmail.com>
2014-10-28 18:06:10 +0800

    Dirty

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,27 +3,25 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-   //  	$value = '{hasMore:true, data:[{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
-			// {img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"}]}';
-   //  	$this->assign('data', $value);
-   //      $this->display('Html:index');
-
-    	$this->display('Html:shop_home');
+    	$value = '{hasMore:true, data:[{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"},
+			{img:"resources/home/images/sample.jpg", title:"2015 春季时装秀掠影", text:"备受期待的路易威登 2015 春季女士时装秀在位于巴黎的路易威登基金会大楼举办。", date: "10月18日", cat: "时尚"}]}';
+    	$this->assign('data', $value);
+        $this->display('Html:index');
     }
 }
