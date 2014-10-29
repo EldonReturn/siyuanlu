7d6d62cf3b89610dcd1a5ea034f7a33b13efcf2a
He Gu Yi <ezcommerce@outlook.com>
2014-10-29 08:05:35 +0800

    Revert "Dojo Init"
    This reverts commit 94c2d1e9f5e36e579dde34c5a39a75b828185d39.

--- resources/home/js/marquee.js
+++ resources/home/js/marquee.js
@@ -1,19 +1,15 @@
 (function($){
 	$.Marquee = function(){
-		var root = $('<div class="marquee"></div>'),
-			marqueeBox = $('<ul class="marquee_box"></ul>'),
-			masker = $('<div class="mask"><span></span><span></span></div>'),
+		var root = $('<ul class="marquee_box"></ul>'),
 			data = [{img:'resources/home/images/sample_large.jpg'}, {img:'resources/home/images/sample_large.jpg'}],
 			node, image, len = data.length,
 			scrWidth = $(document).width();
 
-		root.append(masker);
-		root.append(marqueeBox);
 		for(var i = 0; i < len; i++){
 			node = $('<li class = "slide"></li>');
 			image = $('<img src="' + data[i].img + '" />');
 			node.append(image);
-			marqueeBox.append(node);
+			root.append(node);
 			image.load(function(){
 				node.css('left', (scrWidth - node.width()) / 2);
 			});
