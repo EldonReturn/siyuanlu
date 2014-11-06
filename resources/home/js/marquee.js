ff5029727e9d8c5ae767dbe283bf063bcafd53c9
He Gu Yi <ezcommerce@outlook.com>
2014-11-07 07:41:41 +0800

    Updated

--- resources/home/js/marquee.js
+++ resources/home/js/marquee.js
@@ -1,9 +1,9 @@
 (function($){
 	$.Marquee = function(){
 		var root = $('<ul class="marquee_box"></ul>'),
-			data = [{img:'resources/home/images/sample_large.jpg'}],
+			data = [{img:'resources/home/images/sample_large.jpg'}, {img:'resources/home/images/sample_large_2.jpg'}],
 			image, len = data.length,
-			nodes = new Array(len), nodeIndex = 0,
+			nodes = new Array(len), nodeIndex = 1,
 			scrWidth = $(document).width();
 
 		for(var i = 0; i < len; i++){
@@ -21,9 +21,13 @@
 		this.height = 500;
 		root.css('height', this.height + 'px');
 
-		this.rotate = function(){
-			nodeIndex
-			nodes[].animate({opacity:0}, 'fast');
+		this.rotate = function(fwd /*是否正循环*/){
+			nodes[nodeIndex].css('z-index', 9);
+			nodes[nodeIndex].animate({opacity:0}, 'slow', function(){
+				nodes[!fwd ? (nodeIndex + len - 1) % len : (nodeIndex + 1)%len].css({'z-index': 9, 'opacity': 1});
+				nodes[nodeIndex].css({'z-index': 1, 'opacity': 1});
+				nodeIndex = !fwd ? (nodeIndex + len - 1) % len : (nodeIndex + 1)%len;
+			});
 		};
 		this.getNode = function(){
 			return root;
