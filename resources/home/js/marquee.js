33acbada11f18d4a7d9aaa52bd8ef2bcbff43711
He Gu Yi <ezcommerce@outlook.com>
2014-11-11 21:36:53 +0800

    Marquee in good shape

--- resources/home/js/marquee.js
+++ resources/home/js/marquee.js
@@ -1,16 +1,29 @@
 (function($){
 	$.Marquee = function(){
-		var root = $('<ul class="marquee_box"></ul>'),
+		var navHeight = 120,
+			root = $('<div class="marquee_box"></div>'),
+			nav_left = $('<div class="prev display_none"><img src="resources/home/images/marquee/prev.png"/></div>'),
+			nav_right = $('<div class="next display_none"><img src="resources/home/images/marquee/next.png"/></div>'),
+			container = $('<ul class="container"></ul>'),
 			data = [{img:'resources/home/images/sample_large.jpg'}, {img:'resources/home/images/sample_large_2.jpg'}],
 			image, len = data.length,
 			nodes = new Array(len), nodeIndex = 1,
 			scrWidth = $(document).width();
 
+		this.height = 500;
+
+		root.append(nav_left);
+		root.append(nav_right);
+		root.append(container);
+
+		nav_left.css('top', (this.height - navHeight) / 2);
+		nav_right.css('top', (this.height - navHeight) / 2);
+
 		for(var i = 0; i < len; i++){
 			nodes[i] = $('<li class = "slide"></li>');
 			image = $('<img src="' + data[i].img + '" />');
 			nodes[i].append(image);
-			root.append(nodes[i]);
+			container.append(nodes[i]);
 			image.load((function(index){
 				return function(){
 					nodes[index].css('left', (scrWidth - nodes[index].width()) / 2);
@@ -18,8 +31,37 @@
 			})(i));
 		}
 
-		this.height = 500;
-		root.css('height', this.height + 'px');
+		container.css('height', this.height + 'px');
+
+		// 监测窗口大小改变
+		$(window).resize(function(){
+			scrWidth = $(document).width();
+			for(var i = 0; i < len; i++){
+				nodes[i].css('left', (scrWidth - nodes[i].width()) / 2);
+			}
+		});
+
+		// 监测鼠标是否在上面
+		root.mouseover(function(){
+			nav_left.removeClass('display_none');
+			nav_right.removeClass('display_none');
+		});
+		root.mouseout(function(){
+			nav_left.removeClass('display_none').addClass('display_none');
+			nav_right.removeClass('display_none').addClass('display_none');
+		});
+
+		// 监测左右浏览事件
+		nav_left.click((function(owner){
+			return function(){
+				owner.rotate();
+			};
+		})(this));
+		nav_right.click((function(owner){
+			return function(){
+				owner.rotate(true);
+			};
+		})(this));
 
 		this.rotate = function(fwd /*是否正循环*/){
 			nodes[nodeIndex].css('z-index', 9);
